4fc66e48b468f67686a22df7e1dbdb3c7a1dcccb
Piyush Katariya <47279598+Piyush2912@users.noreply.github.com>
2026-08-31 13:02:59 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,93 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // HashSet Length | T(n)=O(n), S(n)=O(n)
+        return Arrays.stream(nums).distinct().count() < nums.length;        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // HashSet | T(n):O(n), S(n):O(n)
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i: nums) {
+        //     if (set.contains(i)) {
+        //         return true;
+        //     }
+        //     set.add(i);
+        // }        
+        // return false;
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+        // Sorting and Adjacent comparison | T(n):O(nlogn), S(n):O(1)
+        // Arrays.sort(nums);
+        // for(int i=1; i<nums.length; i++) {
+        //     if (nums[i-1] == nums[i]) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        // Brute Force: T(n) = O(n), S(n)=O(1)
+        // int n = nums.length;
+        // for (int i=0; i<n-1; i++) {
+        //     for (int j=i+1; j<n; j++) {
+        //         if (nums[i] == nums[j]) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+    }
+}
